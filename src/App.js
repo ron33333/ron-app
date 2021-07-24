3256d1ab824a4b3cd952ea7d0b459f046e449637
ron33333 <86957511+ron33333@users.noreply.github.com>
2021-07-24 20:26:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Avatar from "./Components/Avatar";
 
 function App() {
+  const URL='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Avatar url={URL}/>
+   </div>
   );
 }
+
 
 export default App;
